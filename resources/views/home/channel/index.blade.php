a930b73aba11440f38300c8e0ac71987b0010dd1
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2021-06-26 23:36:39 +0700

    Update index.blade.php

--- resources/views/home/channel/index.blade.php
+++ resources/views/home/channel/index.blade.php
@@ -110,11 +110,8 @@
                                     </td>
                                     <td>
                                         {{$values[$i][4]}}
-                                        @php
-                                         dd($values[$i][5]);   
-                                        @endphp
                                     </td>
-                                    <td>{{number_format($values[$i][5], 0, ',', '.')}} <br/>
+                                    <td>{{$values[$i][5]}} <br/>
                                         @if (!isset($values[$i][6]))
                                         <button onclick="go()" style="background-color: #E1CE69; border: 1px solid #E1CE69;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                         0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Đặt mua</button>
