fc9a5efa5213995bd599a1caebe1aff83c150f61
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2021-06-28 13:12:54 +0700

    Update consultation.blade.php

--- resources/views/home/includes/consultation.blade.php
+++ resources/views/home/includes/consultation.blade.php
@@ -5,12 +5,12 @@
             <div class="col-12 col-md-10 col-lx-8">
                 <div class="box-action row">
                     <div class="box-action--content col-12 col-md-6">
-                        <h3 class="title" data-cms="{{app()->getLocale()}}-includes-consultation-1">REQUEST CONSULTATION</h3>
-                        <div class="sapo" data-cms="{{app()->getLocale()}}-includes-consultation-2">Subscribe now and receive the latest news and updates!</div>
+                        <h3 class="title" data-cms="{{app()->getLocale()}}-includes-consultation-1">Bạn cần sự tư vấn</h3>
+                        <div class="sapo" data-cms="{{app()->getLocale()}}-includes-consultation-2">Hãy liên hệ với chúng tôi!</div>
                     </div>
                     <div class="box-action--form col-12 col-md-6 d-flex align-items-center">
                         <form class="form-group js-form">
-                            <input type="text" class="form-control" name="email" placeholder="@lang('Your email')" required>
+                            <input type="text" class="form-control" name="phone" placeholder="Số điện thoại của bạn" required>
                             <button type="submit" class="btn btn-form">
                                 <svg class="icon"><use xlink:href="#icon-send"></use></svg>
                                 <span>@lang('Send')</span>
