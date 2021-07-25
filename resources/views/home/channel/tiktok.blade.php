ed5a768f5503258d0d022ea34aac4417353b0a24
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2021-07-25 10:28:53 +0700

    a

--- resources/views/home/channel/tiktok.blade.php
+++ resources/views/home/channel/tiktok.blade.php
@@ -88,7 +88,6 @@
                                 <th>{{$values[0][4]}}</th>
                                 <th>{{$values[0][5]}}</th>
                                 <th>{{$values[0][6]}}</th>
-                                <th>{{$values[0][7]}}</th>
                             </tr>
                         </thead>
 
@@ -121,8 +120,6 @@
                                     </td>
                                     <td>
                                         {{$values[$i][6]}}
-                                    </td>\<td>
-                                        {{$values[$i][7]}}
                                     </td>
                                 </tr>
                             @endfor
