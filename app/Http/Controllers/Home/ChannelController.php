00f56ac16835a62ae4b14061e803c02894006784
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2021-07-25 10:22:54 +0700

    Update ChannelController.php

--- app/Http/Controllers/Home/ChannelController.php
+++ app/Http/Controllers/Home/ChannelController.php
@@ -23,7 +23,7 @@ class ChannelController extends Controller
 		$response = $service->spreadsheets_values->get($spreadsheetId, $range);
 		$arr = $response->getValues();
         $values = [];
-
+        dd($arr);
         foreach($arr as $key=>$item){
             if($key == 1){
                 $values[] = $item;
